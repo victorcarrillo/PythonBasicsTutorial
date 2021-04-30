93f85cfcdd02fa78d0595b358e93782f14294eeb
Victor Carrillo <victorcarrillo@Macbookwork-Victor.local>
2021-04-30 17:21:05 -0500

    Adding first tutorials

---
+++ Basics/PythonConditionals.py
@@ -0,0 +1,9 @@
+
+if 5 > 2:
+    print (" 5 is greater than 2")
+
+num = 1
+if num > 0:
+    print ("this is a positive number")
+else:
+    print ("this is a negative number")
---
+++ README.md
@@ -0,0 +1,11 @@
+Python Basics Tutorial
+
+1. Basics
+2. Variables
+3. Syntax
+4. Function
+5. CommandLine
+6. Comments
+7. Numbers
+8. Strings
+9. Input
